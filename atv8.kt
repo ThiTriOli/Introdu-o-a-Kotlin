26631ae0441d28e54088c8ad6efebaad75fb107e
ThiTriOli <159439370+ThiTriOli@users.noreply.github.com>
2025-02-12 11:28:58 -0300

    Update atv8.kt

--- atv8.kt
+++ atv8.kt
@@ -1,4 +1,4 @@
-//atv8//
+//atv8-Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.//
 fun main() {
     println("Digite o valor por hora: ")
     val valorH = readLine()!!.toInt()
