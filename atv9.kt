74b3869b5d7a3b66ee01e97e21b3f17f6b5462f8
ThiTriOli <159439370+ThiTriOli@users.noreply.github.com>
2025-02-12 11:29:10 -0300

    Update atv9.kt

--- atv9.kt
+++ atv9.kt
@@ -1,4 +1,4 @@
-//atv9//
+//atv9-Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.//
 fun main() {
     println("Digite o valor em Fahrenheit: ")
     val F = readLine()!!.toDouble()
