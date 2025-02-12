99ffcfe7141e3baf0950801e04e9d52c4fd837de
ThiTriOli <159439370+ThiTriOli@users.noreply.github.com>
2025-02-12 11:28:37 -0300

    Update atv7.kt

--- atv7.kt
+++ atv7.kt
@@ -1,4 +1,4 @@
-//atv7//
+//atv7-Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário//
 fun main() {
     println("Digite o lado: ")
     val lado = readLine()!!.toInt()
