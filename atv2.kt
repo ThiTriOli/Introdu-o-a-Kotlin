92274738fc9c4a2cf8500f23ed415bfc7d9199d9
ThiTriOli <159439370+ThiTriOli@users.noreply.github.com>
2025-02-12 11:27:08 -0300

    Update atv2.kt

--- atv2.kt
+++ atv2.kt
@@ -1,4 +1,4 @@
-//atv2//
+//atv2-Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].//
 fun main(args: Array<String>) {
 println("Digite um número:")
 val numero = readLine()!!.toInt()
